b23875dec4c733649de6efd67e975b55d7e7a194
srjoy5000 <riosato2000@gmail.com>
2026-05-11 14:01:48 +0900

    add neetcode-150

---
+++ neetcode-150/arrays-and-hashing/217-contains-duplicate.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  bool hasDuplicate(vector<int>& nums) {
+    unordered_set<int> s(nums.begin(), nums.end());
+    return s.size() != nums.size();
+  }
+};
