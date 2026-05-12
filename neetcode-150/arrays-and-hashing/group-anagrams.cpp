84127075033ea1598088aa234ffa7b85c92ca0dd
srjoy5000 <riosato2000@gmail.com>
2026-05-13 08:32:44 +0900

    update new neetcode-150 problem

--- neetcode-150/arrays-and-hashing/group-anagrams.cpp
+++ neetcode-150/arrays-and-hashing/group-anagrams.cpp
@@ -13,16 +13,18 @@ public:
         {
             string str_sorted = strs[i];
             sort(str_sorted.begin(), str_sorted.end());
-            if (seen.find(str_sorted) != seen.end())
-            {
-                seen[str_sorted].push_back(strs[i]);
-            }
-            else
-            {
-                seen[str_sorted].push_back(strs[i]);
-            }
+            seen[str_sorted].push_back(strs[i]);
         }
         vector<vector<string>> ans;
+        // 'pair' is a reference to an entry in your map, but it's just a name.
+        // pair.first  -> The string key (the character counts)
+        // pair.second -> The vector<string> (the list of anagrams)
+        //
+        // Without & : The compiler makes a copy of every item in the collection.
+        // If you have a vector of 1, 000 strings, it will copy those strings one by one, which is slow and wastes memory.
+        //
+        // With &: You are creating a reference (an alias).
+        // It’s like creating a shortcut to the original data. No copying happens, so it is much faster.
         for (auto &pair : seen)
         {
             ans.push_back(pair.second);
