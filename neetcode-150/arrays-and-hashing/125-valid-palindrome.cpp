01ddeb81e36326129ed870c24c2c11a3fda260e5
srjoy5000 <riosato2000@gmail.com>
2026-05-18 11:12:57 +0900

    add: solve new challenge

---
+++ neetcode-150/arrays-and-hashing/125-valid-palindrome.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// first try
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        string str = "";
+        for (char c : s)
+            if (c != ' ' && isalnum(c))
+                str += tolower(c);
+        string orig = str;
+        reverse(str.begin(), str.end());
+        return str == orig;
+    }
+};
+
+// 2nd version
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int l = 0, r = s.size() - 1;
+        while (l < r)
+        {
+            if (!isAlNum(s[l]))
+                l++;
+            else if (!isAlNum(s[r]))
+                r--;
+            else if (tolower(s[l++]) != tolower(s[r--]))
+                return false;
+        }
+        return true;
+    }
+
+    bool isAlNum(char c)
+    {
+        return (c >= 'a' && c <= 'z' ||
+                c >= 'A' && c <= 'Z' ||
+                c >= '0' && c <= '9');
+    }
+};
